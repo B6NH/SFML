eeb9f6e92eea5cf6358ee48f79a684910734562e
Jan Kowalski <yakov85@o2.pl>
2020-04-21 22:29:11 +0200

    Move Rectangle

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <SFML/Graphics.hpp>
 
+// g++ -c main.cpp && g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system && ./sfml-app
+
 int main()
 {
+
+    const float SPEED = 40;
+
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    window.setFramerateLimit(20);
+
+    sf::RectangleShape square(sf::Vector2f(60, 60));
+    square.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
@@ -13,10 +20,24 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            else if(event.type == sf::Event::KeyPressed){
+              if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+                square.move(-SPEED,0);
+              }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+                square.move(SPEED,0);
+              }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+                square.move(0,-SPEED);
+              }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+                square.move(0,SPEED);
+              }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+                window.close();
+              }
+              //square.setFillColor(sf::Color::Red);
+            }
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(square);
         window.display();
     }
 
