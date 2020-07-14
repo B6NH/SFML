7d4e83b9eaa1bc2d183ff94c28a11a0b181bfd73
Jan Kowalski <yakov85@o2.pl>
2020-07-14 23:30:31 +0200

    Format

--- main.cpp
+++ main.cpp
@@ -11,29 +11,31 @@ int main()
 {
 
     sf::RenderWindow window(sf::VideoMode(200, 200), "Test");
+
     window.setFramerateLimit(20);
 
     World world;
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            else if(event.type == sf::Event::KeyPressed){
-              if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
-                window.close();
-              }else{
-                world.Key();
-              }
-            }
-        }
+    while(window.isOpen()){
 
-        window.clear();
-        world.DrawOn(window);
-        window.display();
+      sf::Event event;
+
+      while(window.pollEvent(event)){
+        if(event.type == sf::Event::Closed){
+          window.close();
+        }else if(event.type == sf::Event::KeyPressed){
+          if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+            window.close();
+          }else{
+            world.Key();
+          }
+        }
+      }
+
+      window.clear();
+      world.DrawOn(window);
+      window.display();
+
     }
 
     return 0;
